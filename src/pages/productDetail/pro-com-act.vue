16b0655680fa7db7b9c2e86f40139bff63f2ded2
A0012406 <15254205681@163.com>
2020-07-16 15:45:34 +0800

    修改页面缩进等

--- src/pages/productDetail/pro-com-act.vue
+++ src/pages/productDetail/pro-com-act.vue
@@ -1,252 +1,194 @@
 <template>
-    <view v-show="isShowAct">
-        <view class="mask" @click="close"></view>
-        <view class="popup">
-            <view class="top" @click="close">
-                <view style="color: #333333; font-size: 15px;">营销活动</view>
-                <view style="color: #ED2856;">X</view>
+  <view v-show="isShowAct">
+    <view class="mask" @click="close"></view>
+    <view class="popup">
+      <view class="top" @click="close">
+        <view style="color: #333333; font-size: 15px;">营销活动</view>
+        <view style="color: #ED2856;">X</view>
+      </view>
+      <view class="sorrowC">
+        <view class="lineHigt"></view>
+        <view class="textTalRow" v-for="(act,indexA) in info" :key="act.name">
+          <view class="uni-flex uni-row">
+            <view class="text col-90 borderLeft">
+              <view class="afterr">{{act.name}}</view>
             </view>
-            <view class="content sorrowC">
-                <view class="lineHigt"></view>
-                <view class="uni-flex uni-row textTalRow">
-                    <view class="text col-90 borderLeft">
-                        <view class="afterr">特价版本</view>
-                    </view>
-                    <view class="text borderLeft">2</view>
-                </view>
-                <view class="textTalRow" @click="checkAct('11')">
-                    <view class="uni-flex uni-row textSenRow">
-                        <view class="textRow col-60">名称：TJ2020033333</view>
-                        <view class="textRow col-40">价格：¥ 5949.71</view>
-                    </view>
-                    <view class="uni-flex uni-row textSenRow">
-                        <view class="textRow col-40">有效期：2021-01-01</view>
-                        <view class="textRow col-25">直扣：0.00</view>
-                        <view class="textRow col-33">数量：0.00</view>
-                    </view>
-                </view>
-                <view class="lineHigt"></view>
-                <view class="uni-flex uni-row textTalRow">
-                    <view class="text col-90 borderLeft">
-                        <view class="afterr">特价版本</view>
-                    </view>
-                    <view class="text borderLeft">2</view>
-                </view>
-                <view class="textTalRow">
-                    <view class="uni-flex uni-row textSenRow">
-                        <view class="textRow col-60">名称：TJ2020033333</view>
-                        <view class="textRow col-40">价格：¥ 5949.71</view>
-                    </view>
-                    <view class="uni-flex uni-row textSenRow">
-                        <view class="textRow col-40">有效期：2021-01-01</view>
-                        <view class="textRow col-25">直扣：0.00</view>
-                        <view class="textRow col-33">数量：0.00</view>
-                    </view>
-                </view>
-                <view class="textTalRow">
-                    <view class="uni-flex uni-row textSenRow">
-                        <view class="textRow col-60">名称：TJ2020033333</view>
-                        <view class="textRow col-40">价格：¥ 5949.71</view>
-                    </view>
-                    <view class="uni-flex uni-row textSenRow">
-                        <view class="textRow col-40">有效期：2021-01-01</view>
-                        <view class="textRow col-25">直扣：0.00</view>
-                        <view class="textRow col-33">数量：0.00</view>
-                    </view>
-                </view>
-                <view class="lineHigt"></view>
-                <view class="uni-flex uni-row textTalRow">
-                    <view class="text col-90 borderLeft">
-                        <view class="afterr">特价版本</view>
-                    </view>
-                    <view class="text borderLeft">2</view>
-                </view>
-                <view class="textTalRow">
-                    <view class="uni-flex uni-row textSenRow">
-                        <view class="textRow col-60">名称：TJ2020033333</view>
-                        <view class="textRow col-40">价格：¥ 5949.71</view>
-                    </view>
-                    <view class="uni-flex uni-row textSenRow">
-                        <view class="textRow col-40">有效期：2021-01-01</view>
-                        <view class="textRow col-25">直扣：0.00</view>
-                        <view class="textRow col-33">数量：0.00</view>
-                    </view>
-                </view>
-                <view class="lineHigt"></view>
-                <view class="uni-flex uni-row textTalRow">
-                    <view class="text col-90 borderLeft">
-                        <view class="afterr">特价版本</view>
-                    </view>
-                    <view class="text borderLeft">2</view>
-                </view>
-                <view class="textTalRow">
-                    <view class="uni-flex uni-row textSenRow">
-                        <view class="textRow col-60">名称：TJ2020033333</view>
-                        <view class="textRow col-40">价格：¥ 5949.71</view>
-                    </view>
-                    <view class="uni-flex uni-row textSenRow">
-                        <view class="textRow col-40">有效期：2021-01-01</view>
-                        <view class="textRow col-25">直扣：0.00</view>
-                        <view class="textRow col-33">数量：0.00</view>
-                    </view>
-                </view>
-                <view class="lineHigt"></view>
-                <view class="uni-flex uni-row textTalRow">
-                    <view class="text col-90 borderLeft">
-                        <view class="afterr">特价版本</view>
-                    </view>
-                    <view class="text borderLeft">2</view>
-                </view>
-                <view class="textTalRow">
-                    <view class="uni-flex uni-row textSenRow">
-                        <view class="textRow col-60">名称：TJ2020033333</view>
-                        <view class="textRow col-40">价格：¥ 5949.71</view>
-                    </view>
-                    <view class="uni-flex uni-row textSenRow">
-                        <view class="textRow col-40">有效期：2021-01-01</view>
-                        <view class="textRow col-25">直扣：0.00</view>
-                        <view class="textRow col-33">数量：0.00</view>
-                    </view>
-                </view>
+            <view class="text borderLeft iconxia"></view>
+          </view>
+          <view v-for="(ls,index) in act.list" :key="ls.name" class="textTalRow" @click="checkAct(indexA,index)">
+            <view class="uni-flex uni-row textSenRow" :class="{'isNoCheck':!ls.isCheck,'isYesCheckTop':ls.isCheck}">
+              <view class="textRow col-60">{{ls.num}}名称：TJ2020033333</view>
+              <view class="textRow col-40">价格：¥ 5949.71</view>
             </view>
-            <view class="uni-flex uni-row">
-                <view class="col-50">
-                    <input @click="putAct" disabled="disabled" class="button-left col-50" type="button" value="确定"/>
-                </view>
-                <view class="col-50">
-                    <input @click="refresh" disabled="disabled" class="button-right col-50" type="button" value="重置"/>
-                </view>
+            <view class="uni-flex uni-row textSenRow" :class="{'isNoCheck':!ls.isCheck,'isYesCheckBot':ls.isCheck}">
+              <view class="textRow col-40">有效期：2021-01-01</view>
+              <view class="textRow col-25">直扣：0.00</view>
+              <view class="textRow col-33">数量：0.00</view>
             </view>
+          </view>
         </view>
+        <view class="lineHigt"></view>
+      </view>
+      <view class="uni-flex uni-row">
+        <view class="col-50">
+          <input @click="putAct" disabled="disabled" class="button-left col-50" type="button" value="确定"/>
+        </view>
+        <view class="col-50">
+          <input @click="refresh" disabled="disabled" class="button-right col-50" type="button" value="重置"/>
+        </view>
+      </view>
     </view>
+  </view>
 </template>
 
 <script>
-    export default {
-        name: 'proComAct',
-        components: {
-        },
-        props:{//父级传来的数据
-            isShowAct: {//页面是否显示
-                type: Boolean,
-                default: true
-            },
-            info:{//图片以及产品数据
-                type: String,
-                default: 'hello'
-            }
-        },
-        data() {
-            return {
-                numberValue: 0
-            }
-        },
-        methods: {
-            close() {
-                this.$emit("closeAct",'close');
-            },
-            checkAct(value) {//选择活动
-                this.numberValue = value
-            },
-            putAct(){//确认
-                this.$emit("checkedAct",this.numberValue);
-            },
-            refresh(){//重置
-
-            }
-        }
+export default {
+  name: 'proComAct',
+  components: {
+  },
+  props: {// 父级传来的数据
+    isShowAct: {// 页面是否显示
+      type: Boolean,
+      default: true
+    },
+    info: {// 数据
+      type: Array,
+      default() {
+        return [];
+      }
+    },
+  },
+  data() {
+    return {
+      numberValue: 0
     };
+  },
+  methods: {
+    close() {
+      this.$emit('closeAct', 'close');
+    },
+    checkAct(a, b) { // 选择活动
+      for (let i = 0; i < this.info.length; i++) {
+        for (let j = 0; j < this.info[i].list.length; j++) {
+          this.info[i].list[j].isCheck = false;
+        }
+      }
+      console.log(a);
+      console.log(b);
+      this.info[a].list[b].isCheck = true;
+      console.log(this.info);
+    },
+    putAct() { // 确认
+      this.$emit('checkedAct', this.numberValue);
+    },
+    refresh() { // 重置
+
+    }
+  }
+};
 </script>
 
 <style>
-    .mask {
-        position: fixed;
-        z-index: 998;
-        top: 0;
-        right: 0;
-        bottom: 0;
-        left: 0;
-        background-color: rgba(0, 0, 0, 0.3);
-        display: -webkit-flex;
-    }
-    .popup {
-        margin-top: -60%;
-        z-index: 999;
-        background-color: #ffffff;
-        height: 70%;
-        width: 100%;
-        position:fixed;
-    }
-    .sorrowC{
-        overflow-x: hidden;
-        overflow-y: scroll;
-        height: 75%;
-    }
-    .top{
-        width: 92%;
-        margin: 0 4%;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-between;
-        background-color: #FFFFFF;
-    }
-    .button-left {
-        float: right;
-        width: 183px;
-        height: 66px;
-        line-height: 66px;
-        text-align: center;
-        background-color: #ED2856;
-        color: #FFFFFF;
-        margin-top: 10px;
-        margin-bottom: 40px;
-        border-bottom-left-radius: 50px;
-        border-top-left-radius: 50px;
-    }
-    .button-right {
-        width: 183px;
-        height: 66px;
-        line-height: 66px;
-        text-align: center;
-        background-color: #F2F2F7;
-        color: #666666;
-        margin-top: 10px;
-        margin-bottom: 40px;
-        border-bottom-right-radius: 50px;
-        border-top-right-radius: 50px;
-    }
-    .textTalRow {
-        padding-left: 20px;
-        padding-right: 20px;
-        padding-bottom: 10px;
-    }
-    .borderLeft {
-        height: 70px;
-        line-height: 70px;
-    }
-    .afterr {
-        margin-top: 20px;
-        padding-left: 10px;
-        border-left: 6px solid #ED2856;
-        font-size: 24px;
-        color: #ED2856;
-        height: 30px;
-        line-height: 30px;
-    }
-    .textSenRow {
-        padding-left: 20px;
-    }
-    .textRow {
-        text-align: left;
-        color: #999999;
-        font-size: 20px;
-    }
-    .text2 {
-        height: 70px;
-        line-height: 70px;
-    }
-    .lineHigt {
-        background-color: #F0F0F0;
-        height: 2px;
-    }
+  .mask {
+    position: fixed;
+    z-index: 998;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background-color: rgba(0, 0, 0, 0.3);
+    display: -webkit-flex;
+  }
+  .popup {
+    margin-top: -60%;
+    z-index: 999;
+    background-color: #ffffff;
+    height: 70%;
+    width: 100%;
+    position:fixed;
+  }
+  .sorrowC{
+    overflow-x: hidden;
+    overflow-y: scroll;
+    height: 75%;
+  }
+  .top{
+    width: 92%;
+    margin: 0 4%;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    background-color: #FFFFFF;
+  }
+  .button-left {
+    float: right;
+    width: 183px;
+    height: 66px;
+    line-height: 66px;
+    text-align: center;
+    background-color: #ED2856;
+    color: #FFFFFF;
+    margin-top: 10px;
+    margin-bottom: 40px;
+    border-bottom-left-radius: 50px;
+    border-top-left-radius: 50px;
+  }
+  .button-right {
+    width: 183px;
+    height: 66px;
+    line-height: 66px;
+    text-align: center;
+    background-color: #F2F2F7;
+    color: #666666;
+    margin-top: 10px;
+    margin-bottom: 40px;
+    border-bottom-right-radius: 50px;
+    border-top-right-radius: 50px;
+  }
+  .textTalRow {
+    padding-left: 20px;
+    padding-right: 20px;
+    padding-bottom: 10px;
+  }
+  .borderLeft {
+    height: 70px;
+    line-height: 70px;
+  }
+  .afterr {
+    margin-top: 20px;
+    padding-left: 10px;
+    border-left: 6px solid #ED2856;
+    font-size: 24px;
+    color: #ED2856;
+    height: 30px;
+    line-height: 30px;
+  }
+  .textSenRow {
+    padding-left: 20px;
+    padding-bottom: 10px;
+  }
+  .textRow {
+    text-align: left;
+    font-size: 20px;
+  }
+  .lineHigt {
+    background-color: #F0F0F0;
+    height: 2px;
+  }
+  .isNoCheck {
+    background-color: #F2F2F7;
+    color: #999999;
+  }
+  .isYesCheckTop {
+    background-color: #FFF5F7;
+    color: #ED2856;
+    border: 1px #ED2856 solid;
+    border-bottom: none;
+  }
+  .isYesCheckBot {
+    background-color: #FFF5F7;
+    color: #ED2856;
+    border: 1px #ED2856 solid;
+    border-top: none;
+  }
 </style>
--- src/pages/productDetail/pro-com-ship.vue
+++ src/pages/productDetail/pro-com-ship.vue
@@ -1,13 +1,179 @@
 <template>
-    $END$
+  <view v-show="isShowShip">
+    <view class="mask" @click="close"></view>
+    <view class="popup">
+      <view class="top" @click="close">
+        <view style="color: #333333; font-size: 15px;">配送至</view>
+        <view style="color: #ED2856;">X</view>
+      </view>
+      <view class="content sorrowC">
+        <view class="lineHigt"></view>
+        <view class="uni-flex uni-row textTalRow">
+          <view class="text col-90 borderLeft">
+            <view class="afterr">特价版本</view>
+          </view>
+          <view class="text borderLeft">2</view>
+        </view>
+        <view class="textTalRow" @click="checkAct('11')">
+          <view class="uni-flex uni-row textSenRow">
+            <view class="textRow col-60">名称：TJ2020033333</view>
+            <view class="textRow col-40">价格：¥ 5949.71</view>
+          </view>
+          <view class="uni-flex uni-row textSenRow">
+            <view class="textRow col-40">有效期：2021-01-01</view>
+            <view class="textRow col-25">直扣：0.00</view>
+            <view class="textRow col-33">数量：0.00</view>
+          </view>
+        </view>
+        <view class="lineHigt"></view>
+        <view class="uni-flex uni-row textTalRow">
+          <view class="text col-90 borderLeft">
+            <view class="afterr">特价版本</view>
+          </view>
+          <view class="text borderLeft">2</view>
+        </view>
+        <view class="textTalRow">
+          <view class="uni-flex uni-row textSenRow">
+            <view class="textRow col-60">名称：TJ2020033333</view>
+            <view class="textRow col-40">价格：¥ 5949.71</view>
+          </view>
+          <view class="uni-flex uni-row textSenRow">
+            <view class="textRow col-40">有效期：2021-01-01</view>
+            <view class="textRow col-25">直扣：0.00</view>
+            <view class="textRow col-33">数量：0.00</view>
+          </view>
+        </view>
+        <view class="textTalRow">
+          <view class="uni-flex uni-row textSenRow">
+            <view class="textRow col-60">名称：TJ2020033333</view>
+            <view class="textRow col-40">价格：¥ 5949.71</view>
+          </view>
+          <view class="uni-flex uni-row textSenRow">
+            <view class="textRow col-40">有效期：2021-01-01</view>
+            <view class="textRow col-25">直扣：0.00</view>
+            <view class="textRow col-33">数量：0.00</view>
+          </view>
+        </view>
+        <view class="lineHigt"></view>
+        <view class="uni-flex uni-row textTalRow">
+          <view class="text col-90 borderLeft">
+            <view class="afterr">特价版本</view>
+          </view>
+          <view class="text borderLeft">2</view>
+        </view>
+        <view class="textTalRow">
+          <view class="uni-flex uni-row textSenRow">
+            <view class="textRow col-60">名称：TJ2020033333</view>
+            <view class="textRow col-40">价格：¥ 5949.71</view>
+          </view>
+          <view class="uni-flex uni-row textSenRow">
+            <view class="textRow col-40">有效期：2021-01-01</view>
+            <view class="textRow col-25">直扣：0.00</view>
+            <view class="textRow col-33">数量：0.00</view>
+          </view>
+        </view>
+      </view>
+    </view>
+  </view>
 </template>
 
 <script>
-    export default {
-        name: 'pro-com-ship'
+export default {
+  name: 'proComShip',
+  components: {
+  },
+  props: {// 父级传来的数据
+    isShowShip: {// 页面是否显示
+      type: Boolean,
+      default: true
+    },
+    info: {// 图片以及产品数据
+      type: String,
+      default: 'hello'
+    }
+  },
+  data() {
+    return {
+      numberValue: 0
     };
+  },
+  methods: {
+    close() {
+      this.$emit('closeShip', 'close');
+    },
+    checkAct(value) { // 选择活动
+      this.numberValue = value;
+    },
+    putAct() { // 确认
+      this.$emit('checkedShip', this.numberValue);
+    },
+    refresh() { // 重置
+
+    }
+  }
+};
 </script>
 
-<style scoped>
-
+<style>
+  .mask {
+    position: fixed;
+    z-index: 998;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background-color: rgba(0, 0, 0, 0.3);
+    display: -webkit-flex;
+  }
+  .popup {
+    margin-top: -60%;
+    z-index: 999;
+    background-color: #ffffff;
+    height: 70%;
+    width: 100%;
+    position:fixed;
+  }
+  .sorrowC{
+    overflow-x: hidden;
+    overflow-y: scroll;
+    height: 75%;
+  }
+  .top{
+    width: 92%;
+    margin: 0 4%;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    background-color: #FFFFFF;
+  }
+  .textTalRow {
+    padding-left: 20px;
+    padding-right: 20px;
+    padding-bottom: 10px;
+  }
+  .borderLeft {
+    height: 70px;
+    line-height: 70px;
+  }
+  .afterr {
+    margin-top: 20px;
+    padding-left: 10px;
+    border-left: 6px solid #ED2856;
+    font-size: 24px;
+    color: #ED2856;
+    height: 30px;
+    line-height: 30px;
+  }
+  .textSenRow {
+    padding-left: 20px;
+  }
+  .textRow {
+    text-align: left;
+    color: #999999;
+    font-size: 20px;
+  }
+  .lineHigt {
+    background-color: #F0F0F0;
+    height: 2px;
+  }
 </style>
