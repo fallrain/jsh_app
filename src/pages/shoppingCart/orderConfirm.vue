602d0dd03f8e4a4fb829d57997eb804ce85396ba
王广开 <416266475@qq.com>
2020-07-20 15:49:48 +0800

    订单确认及相关组件； 失败订单组件； 余额支付组件；

--- src/pages/shoppingCart/orderConfirm.vue
+++ src/pages/shoppingCart/orderConfirm.vue
@@ -10,23 +10,54 @@
         :goodsList="goods"
       ></j-order-confirm-item>
     </view>
+    <view class="mt24">
+      <j-failure-order-item
+        :goodsList="failureGoodsList"
+      ></j-failure-order-item>
+    </view>
+    <view class="mt24">
+      <j-overage-pay
+      ></j-overage-pay>
+    </view>
+    <view class="orderConfirm-btm j-flex-aic">
+      <view class="orderConfirm-btm-text">实付总额：</view>
+      <view class="orderConfirm-btm-price">¥22383945.12</view>
+      <button
+        type="button"
+        class="orderConfirm-btm-btn ml20"
+      >下一步</button>
+    </view>
   </view>
 </template>
 
 <script>
 import JOrderConfirmAddress from '../../components/shoppingCart/JOrderConfirmAddress';
 import JOrderConfirmItem from '../../components/shoppingCart/JOrderConfirmItem';
+import JFailureOrderItem from '../../components/shoppingCart/JFailureOrderItem';
+import JOveragePay from '../../components/shoppingCart/JOveragePay';
 import './css/orderConfirm.scss';
 
 export default {
   name: 'orderConfirm',
   components: {
+    JOveragePay,
+    JFailureOrderItem,
     JOrderConfirmItem,
     JOrderConfirmAddress
   },
   data() {
     return {
       goodsList: [
+        [
+          {
+            isCreditMode: false
+          },
+          {
+            isCreditMode: false
+          }
+        ]
+      ],
+      failureGoodsList: [
         [
           {
             isCreditMode: false
