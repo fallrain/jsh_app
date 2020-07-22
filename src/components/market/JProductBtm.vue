daf3e428a9466d721c3c269e5656347b362ca733
coolBigSea <haijiao_fan_yt@163.com>
2020-07-22 10:50:24 +0800

    活动详情页面提交

---
+++ src/components/market/JProductBtm.vue
@@ -0,0 +1,47 @@
+<template>
+  <view class="jProductBtm">
+    <view class="jProductBtm-left">
+      <view class="fs24 text-333">
+        合计：<text class="text-theme">￥2893.12</text>
+      </view>
+      <view class="fs20 text-999">
+        剩余可购买套数：<text class="text-theme">94</text>
+      </view>
+    </view>
+    <view class="jProductBtm-right">
+      <uni-number-box></uni-number-box>
+      <button
+        type="button"
+        class="jProductBtm-right-btn"
+      >成套下单</button>
+    </view>
+  </view>
+</template>
+
+<script>
+import {
+  uniNumberBox
+} from '@dcloudio/uni-ui';
+import './css/JProductBtm.scss';
+
+export default {
+  name: 'jProductBtm',
+  components: {
+    uniNumberBox
+  },
+  props: {
+    // 全选
+    checked: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    choose() {
+      const checked = !this.checked;
+      this.$emit('update:checked', checked);
+      this.$emit('checkAll', checked);
+    }
+  }
+};
+</script>
---
+++ src/pages/market/marketDetail.vue
@@ -0,0 +1,134 @@
+<template>
+  <view class="marketDetail">
+    <view class="marketDetail-order-infor mb24">
+      <view
+        class="marketDetail-ads-detail br-b-grey"
+        @tap="showAdsPicker"
+      >
+        <view class="iconfont iconlocal"></view>
+        <text>配送至：青岛市李沧区重庆中路792青岛市李沧区重庆中路792青岛市李沧区重庆中路792</text>
+        <view class="iconfont iconyou right-style"></view>
+      </view>
+      <view class="marketDetail-orders br-b-grey">
+        <view>
+          <view><span class="text-theme fs36">7</span>件</view>
+          <view>已选主产品</view>
+        </view>
+        <view>
+          <view class=""><span class="text-theme fs36">7</span>件</view>
+          <view>已选配比产品</view>
+        </view>
+        <view>
+          <view class=""><span class="text-theme fs36">7</span>件</view>
+          <view>还需主产品</view>
+        </view>
+        <view>
+          <view class=""><span class="text-theme fs36">7</span>件</view>
+          <view>还需配比产品</view>
+        </view>
+      </view>
+      <view class="marketDetail-requirement fs20">
+        <view class="dis-flex mb24">
+          <view class="w230 text-999">主产品起订金额限定</view>
+          <view class="w200 text-333">0.00 / ¥1000.00</view>
+          <view class="w230 text-999">主产品起订数量限定</view>
+          <view class="text-333">7件</view>
+        </view>
+        <view class="dis-flex">
+          <view class="w230 text-999">配比产品起订金额限定</view>
+          <view class="w200 text-333">0.00 / ¥1000.00</view>
+          <view class="w230 text-999">配比产品起订数量限定</view>
+          <view class="text-333">7件</view>
+        </view>
+      </view>
+    </view>
+    <view class="marketDetail-list">
+      <j-product-item
+        v-for="(goods,index) in shoppingList"
+        :key="index"
+        :goods="goods"
+        :index="index"
+        @change="goodsChange"
+      ></j-product-item>
+    </view>
+    <j-product-btm
+    ></j-product-btm>
+    <j-address-picker
+      :show.sync="isShowAdsPicker"
+    ></j-address-picker>
+  </view>
+</template>
+<script>
+import JProductItem from '../../components/market/JProductItem';
+import JProductBtm from '../../components/market/JProductBtm';
+import JFailureGoodsList from '../../components/shoppingCart/JFailureGoodsList';
+import JAddressPicker from '../../components/shoppingCart/JAddressPicker';
+import './css/marketDetail.scss';
+
+
+export default {
+  name: 'marketDetail',
+  components: {
+    JAddressPicker,
+    JFailureGoodsList,
+    JProductItem,
+    JProductBtm
+  },
+  data() {
+    return {
+      shoppingList: [
+        {
+          checked: false,
+          isCreditMode: false
+        },
+        {
+          checked: true,
+          isCreditMode: false
+        },
+        {
+          checked: false,
+          isCreditMode: false
+        },
+        {
+          checked: false,
+          isCreditMode: false
+        }
+      ],
+      failureGoodsList: [
+        {
+          checked: false,
+        },
+        {
+          checked: false,
+        },
+        {
+          checked: false,
+        }
+      ],
+      // 是否全选
+      isCheckAll: false,
+      // 是否显示配送地址选择
+      isShowAdsPicker: false
+    };
+  },
+  methods: {
+    goodsChange(goods, index) {
+      this.shoppingList[index] = goods;
+      this.shoppingList = JSON.parse(JSON.stringify(this.shoppingList));
+    },
+    checkAll(checked) {
+      /* 全部选择回调函数 */
+      this.shoppingList.forEach((v) => {
+        v.checked = checked;
+      });
+    },
+    failureGoodsListChange(list) {
+      this.failureGoodsList = list;
+    },
+    showAdsPicker() {
+      /* 地址选择展示 */
+      this.isShowAdsPicker = true;
+    }
+  }
+};
+</script>
