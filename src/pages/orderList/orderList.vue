311983266c47aec3905326702b6999d4d8b9ea1f
huangMingCui <15254205681@163.com>
2020-07-23 10:50:19 +0800

    A0038733/HMC---订单列表头部tab标签添加

--- src/pages/orderList/orderList.vue
+++ src/pages/orderList/orderList.vue
@@ -1,7 +1,7 @@
 <template>
   <view class="orList">
     <view class="padding-8">
-      <view>111</view>
+      <view><j-tab :tabs="tabs" :hasRightSlot="true" @tabClick="tabClick"></j-tab></view>
       <order-list-item v-for="(iten,index) in productList" :key="index" :info="iten" :index="index" @goDetail="goDetail"></order-list-item>
     </view>
   </view>
@@ -9,15 +9,64 @@
 
 <script>
 import orderListItem from '../../components/orderList/order-list-item';
+import JTab from '../../components/common/JTab';
 
 export default {
   name: 'orderList',
   components: {
-    orderListItem
+    orderListItem,
+    JTab
   },
   data() {
     return {
-      productList: [{ name: '111' }, { name: '121' }, { name: '131' }]
+      productList: [{ name: '111' }, { name: '121' }, { name: '131' }],
+      tabs: [
+        {
+          id: 'qbdn',
+          name: '全部订单',
+          active: true
+        },
+        {
+          id: 'dkk',
+          name: '待扣款',
+          active: false
+        },
+        {
+          id: 'dfh',
+          name: '待发货',
+          active: false
+        },
+        {
+          id: 'yfh',
+          name: '已发货',
+          active: false
+        },
+        {
+          id: 'yqs',
+          name: '已签收',
+          active: false
+        },
+        {
+          id: 'ykp',
+          name: '已开票',
+          active: false
+        },
+        {
+          id: 'yqx',
+          name: '已取消',
+          active: false
+        },
+        {
+          id: 'wljd',
+          name: '物流拒单',
+          active: false
+        },
+        {
+          id: 'thdd',
+          name: '退货订单',
+          active: false
+        }
+      ],
     };
   },
   methods: {
@@ -25,6 +74,10 @@ export default {
       uni.navigateTo({
         url: '/pages/orderList/orderDetail?id=1&name=uniapp'
       });
+      console.log(e);
+    },
+    tabClick(e) {
+      this.tabs = e;
       console.log(e);
     }
   }
