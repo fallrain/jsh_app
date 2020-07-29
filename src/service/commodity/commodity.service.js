4b3e50082f2284c302e87c5baf676a41ec62ec91
王广开 <416266475@qq.com>
2020-07-29 10:10:59 +0800

    价格、收藏的请求；

--- src/service/commodity/commodity.service.js
+++ src/service/commodity/commodity.service.js
@@ -8,4 +8,12 @@ export default {
     /* 商品列表 */
     return jPost(urls.goodsList, data, { noLoading: true });
   },
+  getAllPrice(data) {
+    /* 商品价格 */
+    return jPost(urls.getAllPrice, data);
+  },
+  getStock(data) {
+    /* 商品库存 */
+    return jPost(urls.getStock, data);
+  },
 };
--- src/service/url/customer.url.js
+++ src/service/url/customer.url.js
@@ -3,7 +3,9 @@ import util from '../util/index';
 const baseURL = 'customer';
 const urls = {
   // 送达方地址列表
-  addressesList: 'customers/auxiliary/addresses',
+  addressesList: '/customers/auxiliary/addresses',
+  // 收藏的商品的数据
+  queryCustomerInterestProductByAccount: '/customer/queryCustomerInterestProductByAccount',
 };
 util.addPrefix(baseURL, urls);
 export default urls;
