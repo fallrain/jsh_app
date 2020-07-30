3fc31adc392c09021f643abab119a9d10b8cb9ea
王广开 <416266475@qq.com>
2020-07-30 11:18:44 +0800

    购物车服务； 服务添加注释；

--- src/service/index.js
+++ src/service/index.js
@@ -6,17 +6,28 @@ import mineServer from './mine/mine.server';
 import customerService from './customer/customer.service';
 import catalogServer from './category/category.service';
 import orderServer from './order/order.server';
+import cartService from './cart/cart.service';
 
 export default {
   install(Vue) {
     // 全局引用
+    // 商品服务
     Vue.prototype.commodityService = commodityService;
+    // 营销活动服务
     Vue.prototype.marketService = marketService;
+    // 消息服务
     Vue.prototype.messageService = messageService;
+    // 产品详情服务
     Vue.prototype.productDetailService = productDetailService;
+    // 我的服务
     Vue.prototype.mineServer = mineServer;
+    // 用户服务
     Vue.prototype.customerService = customerService;
+    // 分类服务
     Vue.prototype.catalogServer = catalogServer;
+    // 订单服务
     Vue.prototype.orderServer = orderServer;
+    // 购物车服务
+    Vue.prototype.cartService = cartService;
   }
 };
