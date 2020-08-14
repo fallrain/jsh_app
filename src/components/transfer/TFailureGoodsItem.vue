630fb642e9dd6f792f064f9265d356a6167e1fe6
A0012406 <15254205681@163.com>
2020-08-14 14:24:15 +0800

    A0038733/HMC---调货修改

--- src/components/transfer/TFailureGoodsItem.vue
+++ src/components/transfer/TFailureGoodsItem.vue
@@ -1,50 +1,61 @@
 <template>
   <view class="tFailureGoodsItem">
-    <view 
-      class="tFailureGoodsItem-list"
-      v-for="(order,index) in itemList.data.orderList"
-      :key="index"
-    >
-      <view
-      class="tFailureGoodsItem-cnt-check"
-      @tap="choose(order)"
-      >
-        <i :class="['iconfont', order.checked ? 'iconradio active':'iconradio1']"></i>
-      </view>
-      <view 
-        class="tFailureGoodsItem-cnt-img-wrap"
-      
-      >
-        <image :src="order.THUMBNAIL"></image>
-      </view>
-      <view class="tFailureGoodsItem-cnt">
-        <view class="tFailureGoodsItem-cnt-head">
-          <view class="tFailureGoodsItem-cnt-head-tag mr1">失效</view>
-          <text class="tFailureGoodsItem-cnt-head-text">{{order.PRODUCTNAME}}</text>
+    <view v-if="itemList&&itemList.typpe==='ZC'">
+      <view class="tFailureGoodsItem-list" v-for="(order,index) in itemList.orderList" :key="index">
+        <view class="tFailureGoodsItem-cnt-check" @tap="choose(order)">
+          <i :class="['iconfont', order.checked ? 'iconradio active':'iconradio1']"></i>
         </view>
-        <view class="tFailureGoodsItem-cnt-btm">
-          <text class="tFailureGoodsItem-cnt-head-text">¥ {{order.SUMMONEY}}</text>
-          <text class="tFailureGoodsItem-cnt-head-inf-mrr">付款方</text>
-          <i 
-            class="iconfont iconxia"
-             :class="[
-                  order.isExpand && 'reverse'
-                ]"
-                @tap="showPayer(order,index)"
-          ></i>
-          <view class="tFailureGoodsItem-cnt-price-info" v-show="order.isExpand">
-            <view class="tFailureGoodsItem-cnt-price-info-li"
-              v-for="(it,index) in order.payer"
-              :key="index"
-              :class="[order.isChecked && 'active']"
-              @tap="togglePayer(order, it, index)"  
-            >
-              ({{it.payerCode}}){{it.payerName}}
-            </view>
+        <view class="tFailureGoodsItem-cnt-img-wrap">
+          <image :src="order.THUMBNAIL"></image>
+        </view>
+        <view class="tFailureGoodsItem-cnt">
+          <view class="tFailureGoodsItem-cnt-head">
+            <view class="tFailureGoodsItem-cnt-head-tag mr1">失效</view>
+            <text class="tFailureGoodsItem-cnt-head-text">{{order.PRODUCTNAME}}</text>
           </view>
-          <text class="tFailureGoodsItem-cnt-head-choose">请选择付款方</text>
+          <view class="tFailureGoodsItem-cnt-btm">
+            <text class="tFailureGoodsItem-cnt-head-text">¥ {{order.SUMMONEY}}</text>
+            <text class="tFailureGoodsItem-cnt-head-inf-mrr">付款方</text>
+            <i class="iconfont iconxia" :class="[order.isExpand && 'reverse']" @tap="showPayer(order,index)"></i>
+            <view class="tFailureGoodsItem-cnt-price-info" v-show="order.isExpand">
+              <view class="tFailureGoodsItem-cnt-price-info-li" v-for="(it,index) in order.payer" :key="index"
+                    :class="[order.isChecked && 'active']" @tap="togglePayer(order, it, index)">
+                ({{it.payerCode}}){{it.payerName}}
+              </view>
+            </view>
+            <text class="tFailureGoodsItem-cnt-head-choose">请选择付款方</text>
+          </view>
+          <view class="tFailureGoodsItem-cnt-head-inf-reason">{{order.SXREASON}}</view>
         </view>
-        <view class="tFailureGoodsItem-cnt-head-inf-reason">{{order.SXREASON}}</view>
+      </view>
+    </view>
+    <view v-else>
+      <view class="tFailureGoodsItem-list" v-for="(order,index) in itemList.data.orderList" :key="index">
+        <view class="tFailureGoodsItem-cnt-check" @tap="choose(order)">
+          <i :class="['iconfont', order.checked ? 'iconradio active':'iconradio1']"></i>
+        </view>
+        <view class="tFailureGoodsItem-cnt-img-wrap">
+          <image :src="order.THUMBNAIL"></image>
+        </view>
+        <view class="tFailureGoodsItem-cnt">
+          <view class="tFailureGoodsItem-cnt-head">
+            <view class="tFailureGoodsItem-cnt-head-tag mr1">失效</view>
+            <text class="tFailureGoodsItem-cnt-head-text">{{order.PRODUCTNAME}}</text>
+          </view>
+          <view class="tFailureGoodsItem-cnt-btm">
+            <text class="tFailureGoodsItem-cnt-head-text">¥ {{order.SUMMONEY}}</text>
+            <text class="tFailureGoodsItem-cnt-head-inf-mrr">付款方</text>
+            <i class="iconfont iconxia" :class="[order.isExpand && 'reverse']" @tap="showPayer(order,index)"></i>
+            <view class="tFailureGoodsItem-cnt-price-info" v-show="order.isExpand">
+              <view class="tFailureGoodsItem-cnt-price-info-li" v-for="(it,index) in order.payer" :key="index"
+                    :class="[order.isChecked && 'active']" @tap="togglePayer(order, it, index)">
+                ({{it.payerCode}}){{it.payerName}}
+              </view>
+            </view>
+            <text class="tFailureGoodsItem-cnt-head-choose">请选择付款方</text>
+          </view>
+          <view class="tFailureGoodsItem-cnt-head-inf-reason">{{order.SXREASON}}</view>
+        </view>
       </view>
     </view>
   </view>
@@ -52,9 +63,8 @@
 
 <script>
 export default {
-  name: 'TFailureGoodsItem', 
+  name: 'TFailureGoodsItem',
   props: {
-    
     // 数据
     itemList: {
       type: Object,
@@ -71,7 +81,7 @@ export default {
     };
   },
   created() {
-    
+
   },
   methods: {
     choose(order) {
@@ -80,14 +90,14 @@ export default {
       this.$emit('update:checked', this.itemList.checked);
       this.$emit('change', this.itemList, this.index);
     },
-    showPayer(item,index) {
-     item.isExpand = !item.isExpand
-      console.log(item.isExpand)
+    showPayer(item, index) {
+      item.isExpand = !item.isExpand;
+      console.log(item.isExpand);
       this.$emit('change', this.list, item, this.index);
     },
-      // 切换付款方
+    // 切换付款方
     async togglePayer(item, it, index) {
-      const upDHPay = await this.transfergoodsService.upDHPayMoney ({
+      const upDHPay = await this.transfergoodsService.upDHPayMoney({
         timestamp: Date.parse(new Date()),
         longfeiUSERID: 8700010462,
         ACTPRICE: item.SUMMONEY,
@@ -98,29 +108,29 @@ export default {
         QTY: item.IBL_NUM,
         PAYTO: 8700010462,
         PAYTONAME: item.IBL_PAYMONEYNAME,
-        PROCODE: "",
-        PROLOSSMONEY: "",
+        PROCODE: '',
+        PROLOSSMONEY: '',
         RETAILPRICE: 2449.0000,
-        UNITPRICE: "2449.0000",
-        RELOSERATE: "0.0000",
-        VERCODE: "",
-        VERMONEY: "",
-        REBATEMONEY: "0.0000",
-        IBL_PAYTO_TYPE: "00",
+        UNITPRICE: '2449.0000',
+        RELOSERATE: '0.0000',
+        VERCODE: '',
+        VERMONEY: '',
+        REBATEMONEY: '0.0000',
+        IBL_PAYTO_TYPE: '00',
       });
-      if(upDHPay.code === "1" ) {
+      if (upDHPay.code === '1') {
         // console.log(data)
-        console.log(this.itemList.data.orderList)
-        this.itemList.data.orderList.map(ele => {           
-              ele.isChecked = false
-             console.log(ele.isChecked)   
-          })
-          item.isExpand = !item.isExpand
-          item.isChecked = true
-          console.log(item)
-          document.querySelector(".tFailureGoodsItem-cnt-head-choose").innerHTML =  "(" + it.payerCode + ")" + it.payerName
-          console.log(item.isChecked)
-        this.$emit('change', this.itemList,index);
+        console.log(this.itemList.data.orderList);
+        this.itemList.data.orderList.map((ele) => {
+          ele.isChecked = false;
+          console.log(ele.isChecked);
+        });
+        item.isExpand = !item.isExpand;
+        item.isChecked = true;
+        console.log(item);
+        document.querySelector('.tFailureGoodsItem-cnt-head-choose').innerHTML = `(${it.payerCode})${it.payerName}`;
+        console.log(item.isChecked);
+        this.$emit('change', this.itemList, index);
       }
     }
   }
@@ -130,10 +140,10 @@ export default {
 <style lang="scss">
   .tFailureGoodsItem-cnt-check {
     color: #CFCFCF;
-    
+
   }
   .tFailureGoodsItem {
-    position: relative;  
+    position: relative;
     padding-top: 20px;
     // padding-bottom: 20px;
     // border-bottom: 1px solid #EDE9E9;
@@ -217,7 +227,7 @@ export default {
         margin:10px 0px 10px 20px;
           &.active {
             color: #ED2856;
-            background: #FFF5F7;  
+            background: #FFF5F7;
           }
       }
       .tFailureGoodsItem-cnt-head-choose {
@@ -228,7 +238,7 @@ export default {
       display: inline-block;
       transform: rotateX(180deg);
     }
-  
+
   }
  }
  .tFailureGoodsItem-cnt-head-inf-reason{
