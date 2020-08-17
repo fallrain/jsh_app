f53a08a37421b6c31d6c557743a3ae6a77c5ce5a
王广开 <416266475@qq.com>
2020-08-17 17:08:29 +0800

    抽离送达方至action；

--- src/store/user/index.js
+++ src/store/user/index.js
@@ -9,18 +9,38 @@ export default {
   state: {
     // userInfo为模拟的用户信息
     userInfo: {
-    }
+    },
+    // 售达方信息
+    saleInfo: {}
   },
   mutations: {
     /* eslint注释请不要删除 */
     /* eslint-disable no-unused-expressions */
     [USER.UPDATE_USER](state, data) {
+      /* 修改用户信息 */
       state.userInfo = data;
+    },
+    [USER.UPDATE_SALE](state, data) {
+      /* 修改送达方信息 */
+      state.userInfo = data;
+    }
+  },
+  actions: {
+    async [USER.UPDATE_SALE_ASYNC]({ commit }) {
+      /* 获取售达方信息 */
+      const { code, data } = await Vue.prototype.customerService.getCustomer();
+      if (code === '1') {
+        return commit(USER.UPDATE_SALE, data);
+      }
     }
   },
   getters: {
     [USER.GET_USER](state) {
       /* 用户信息 */
+      return state.userInfo;
+    },
+    [USER.GET_SALE](state) {
+      /* 售达方信息 */
       return state.userInfo;
     },
   }
