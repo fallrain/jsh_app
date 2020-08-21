897929cdecc81b7218a629442f419267729eb8af
王广开 <416266475@qq.com>
2020-08-21 10:42:54 +0800

    配置文件修改；

--- src/config/index.js
+++ src/config/index.js
@@ -1,22 +1,21 @@
 export default {
   h5: {
     dev: {
-      // baseUrl: 'https://new.jsh.com',
-      baseUrl: 'http://mall.jsh.com',
+      baseUrl: 'https://mall.jsh.com',
       cBaseUrl: 'https://c.jsh.com',
     },
     sit: {
-      baseUrl: '',
-      cBaseUrl: '',
+      baseUrl: 'https://mall.jsh.com',
+      cBaseUrl: 'https://c.jsh.com',
     },
     prod: {
-      baseUrl: '',
-      cBaseUrl: '',
+      baseUrl: 'https://new.jsh.com',
+      cBaseUrl: 'https://c.jsh.com',
     }
   },
   'mp-alipay': {
     dev: {
-      baseUrl: 'https://new.jsh.com',
+      baseUrl: 'https://mall.jsh.com',
       cBaseUrl: 'https://c.jsh.com',
     },
     sit: {
