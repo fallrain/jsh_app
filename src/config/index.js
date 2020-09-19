59ae71705d0105552dec774db1e7d5528551568f
王广开 <416266475@qq.com>
2020-09-19 15:43:05 +0800

    版本号；

--- src/config/index.js
+++ src/config/index.js
@@ -4,12 +4,12 @@ export default {
       baseUrl: 'https://mall.jsh.com',
       // baseUrl: 'https://new.jsh.com',
       cBaseUrl: 'https://c-beta.jsh.com',
-      version: '204',
+      version: '207',
     },
     sit: {
       baseUrl: 'https://mall.jsh.com',
       cBaseUrl: 'https://c.jsh.com',
-      version: '204',
+      version: '207',
     },
     prod: {
       baseUrl: 'https://new.jsh.com',
