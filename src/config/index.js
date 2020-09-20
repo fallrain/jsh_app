ef54a8b47b11fac6008678592e3f5202c6ba522c
王广开 <416266475@qq.com>
2020-09-20 13:27:24 +0800

    版本号；

--- src/config/index.js
+++ src/config/index.js
@@ -4,12 +4,12 @@ export default {
       baseUrl: 'https://mall.jsh.com',
       // baseUrl: 'https://new.jsh.com',
       cBaseUrl: 'https://c-beta.jsh.com',
-      version: '207',
+      version: '214',
     },
     sit: {
       baseUrl: 'https://mall.jsh.com',
       cBaseUrl: 'https://c.jsh.com',
-      version: '207',
+      version: '214',
     },
     prod: {
       baseUrl: 'https://new.jsh.com',
